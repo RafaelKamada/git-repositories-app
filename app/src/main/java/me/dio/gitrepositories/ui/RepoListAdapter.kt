0642a326403bc8462b9ae0e22bc7af1d3d7a908f
RKamada <rafael-kamada2007@hotmail.com>
2022-08-07 21:43:08 -0300

    Alterado ícone do App, e adicionado evento para abrir link do repositório

--- app/src/main/java/me/dio/gitrepositories/ui/RepoListAdapter.kt
+++ app/src/main/java/me/dio/gitrepositories/ui/RepoListAdapter.kt
@@ -1,5 +1,7 @@
 package br.com.dio.app.repositories.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -11,6 +13,8 @@ import me.dio.gitrepositories.databinding.ItemRepoBinding
 
 class RepoListAdapter : ListAdapter<Repo, RepoListAdapter.ViewHolder>(DiffCallback()) {
 
+    private var context: android.content.Context? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ItemRepoBinding.inflate(inflater, parent, false)
@@ -18,6 +22,7 @@ class RepoListAdapter : ListAdapter<Repo, RepoListAdapter.ViewHolder>(DiffCallba
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        context = holder.itemView.context
         holder.bind(getItem(position))
     }
 
@@ -30,7 +35,11 @@ class RepoListAdapter : ListAdapter<Repo, RepoListAdapter.ViewHolder>(DiffCallba
             binding.tvRepoDescription.text = item.description
             binding.tvRepoLanguage.text = item.language
             binding.chipStar.text = item.stargazersCount.toString()
-
+            binding.ivOwner.setOnClickListener{
+                val i = Intent(Intent.ACTION_VIEW)
+                i.data = Uri.parse(item.htmlURL)
+                context?.startActivity(i)
+            }
             Glide.with(binding.root.context)
                 .load(item.owner.avatarURL).into(binding.ivOwner)
         }
